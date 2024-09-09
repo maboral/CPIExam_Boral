1f372b9e000c676029ee3b98c95202eb2c788573
Mark Angelo Boral <markangeloboral27@gmail.com>
2024-09-09 13:28:44 +0800

    First Push

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -11,11 +11,20 @@ class PostController extends Controller
 	public function index()
     {
         $posts = Post::all();
-        return view('posts.index', compact('posts'));
+        return view('posts.index')
+					->with('posts', $posts);
     }
 	
 	public function create(Request $request) {
 		return view('posts.create');
+	}
+	
+	public function add() {
+		 $request->validate([
+            'post' => 'required|string|max:255',
+            'description' => 'required|string',
+            'image' => 'nullable|image|mimes:jpeg,png,jpg|max:2048',
+        ]);
 	}
 	
 	public function view(Request $request) {
